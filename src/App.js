ac1571cc2a0dff4850ceae70070ba8fb9a65cbd4
MHaroon3727 <haroonrasheed211f@gmail.com>
2023-10-11 14:12:40 -0700

    Implement local storage functionality for todo list
    - Load todo list items from local storage on component mount
    - Save todo list items to local storage when added or deleted
    - Ensure todo list items persist even after page refresh

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import React, { useState } from 'react'
-import "./App.css"
-import TodoInput from './components/TodoInput'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import TodoInput from './components/TodoInput';
 import Todolist from './components/TodoList';
+
 function App() {
-  const [listTodo,setListTodo]=useState([]);
-  let addList = (inputText)=>{
-    if(inputText!=='')
-      setListTodo([...listTodo,inputText]);
-  }
-  const deleteListItem = (key)=>{
-    let newListTodo = [...listTodo];
-    newListTodo.splice(key,1)
-    setListTodo([...newListTodo])
-  }
+  // Load todo list from local storage or initialize it as an empty array
+  const [listTodo, setListTodo] = useState(() => {
+    const storedTodoList = localStorage.getItem('todoList');
+    return storedTodoList ? JSON.parse(storedTodoList) : [];
+  });
+
+  // Save todo list to local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(listTodo));
+  }, [listTodo]);
+
+  const addList = (inputText) => {
+    if (inputText.trim() !== '') {
+      setListTodo((prevList) => [...prevList, inputText]);
+    }
+  };
+
+  const deleteListItem = (key) => {
+    setListTodo((prevList) => {
+      const newListTodo = [...prevList];
+      newListTodo.splice(key, 1);
+      return newListTodo;
+    });
+  };
+
   return (
     <div className="main-container">
       <div className="center-container">
-      <h1 className="app-heading">TODO LIST</h1>
-        <TodoInput addList={addList}/>
-        <hr/>
-        {listTodo.map((listItem,i)=>{
-          return (
-            <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
-          )
+        <h1 className="app-heading">TODO LIST</h1>
+        <TodoInput addList={addList} />
+        <hr />
+        {listTodo.map((listItem, i) => {
+          return <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem} />;
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
